9962b19f6f8e373142f355eb63609a4813e71d15
amaralbernardo <42038599+amaralbernardo@users.noreply.github.com>
2019-09-06 23:57:27 +0100

    Update Criacao_Layout.R

--- Códigos R/Criacao_Layout.R
+++ Códigos R/Criacao_Layout.R
@@ -1,1 +1,9 @@
+window <- gwindow(titulo) #Janela com o título
+gstatusbar("Autor: Bernardo Amaral", cont=window) #Autor
+getToolkitWidget(window)$maximize() #Maximiza a janela
+BigGroup <- ggroup(cont=window)
+group <- ggroup(horizontal=FALSE, container=BigGroup)
 
+#Adiciona um gráfico na janela
+valor_janela<-novo_ponteiro(ggraphics())
+add(BigGroup,valor_janela$value)
