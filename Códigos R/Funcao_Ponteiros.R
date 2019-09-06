a50bf2d0713e7e6210e11023e8a5ca81dd82e185
amaralbernardo <42038599+amaralbernardo@users.noreply.github.com>
2019-09-06 23:51:31 +0100

    Update Funcao_Ponteiros.R

--- Códigos R/Funcao_Ponteiros.R
+++ Códigos R/Funcao_Ponteiros.R
@@ -1,1 +1,25 @@
+#Função que cria ponteiros. Os ponteiros servem para que as variáveis que tenham esta característica sejam globais dentro do programa até ao seu encerramento
+novo_ponteiro<-function(valor_inserir)
+{  
+  object<-new.env(parent=globalenv())  
+  object$value<-valor_inserir  
+  class(object)<-'pointer'
+  
+  return(object)  
+}
 
+#Função para atualizar o valor do ponteiro
+atualizar_ponteiro<-function (object, ...) 
+{
+  UseMethod("atualizar_ponteiro")   
+}
+
+#Método para atualizar o valor do ponteiro
+atualizar_ponteiro.pointer<-function(object,novo_valor)
+{
+  if (!is(object, "pointer")) 
+  { 
+    stop(" 'object' argument must be of class 'pointer' .") 
+  }   
+  object$value<-novo_valor  
+}
